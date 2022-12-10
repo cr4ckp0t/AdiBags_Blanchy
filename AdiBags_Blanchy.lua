5c4dcf79ac605744bbb75127cf5803f229362f7e
Adam Koch <me@adamko.ch>
2022-12-09 22:01:00 -0500

    Optimized code to fix lag caused by filter.

--- AdiBags_Blanchy.lua
+++ AdiBags_Blanchy.lua
@@ -6,20 +6,6 @@ local addonName, addon = ...
 local AdiBags = LibStub("AceAddon-3.0"):GetAddon("AdiBags")
 
 local L = addon.L
-local tooltip
-
-local function tooltipInit()
-	local tip, leftside = CreateFrame("GameTooltip"), {}
-	for i = 1, 6 do
-		local left, right = tip:CreateFontString(), tip:CreateFontString()
-		left:SetFontObject(GameFontNormal)
-		right:SetFontObject(GameFontNormal)
-		tip:AddFontStrings(left, right)
-		leftside[i] = left
-	end
-	tip.leftside = leftside
-	return tip
-end
 
 local setFilter = AdiBags:RegisterFilter("Sinrunner Blanchy", 98, "ABEvent-1.0")
 setFilter.uiName = L["Sinrunner Blanchy"]
@@ -53,17 +39,4 @@ function setFilter:Filter(slotData)
 	if self.blanchyItems[tonumber(slotData.itemId)] then
 		return L["Sinrunner Blanchy"]
 	end
-
-	--tooltip = tooltip or tooltipInit()
-	tooltip = GameTooltip
-	tooltip:SetOwner(UIParent,"ANCHOR_NONE")
-	tooltip:ClearLines()
-
-	if slotData.bag == BANK_CONTAINER then
-		tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
-	else
-		tooltip:SetBagItem(slotData.bag, slotData.slot)
-	end
-
-	tooltip:Hide()
 end
